9d1f9ffab9895dfa477fe8f6119e5a9b361f8088
home <krishnan.ge@gmail.com>
2026-03-01 11:48:48 +0100

    Initial Commit

---
+++ New LBW/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  New LBW
+//
+//  Created by Home on 3/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ New LBW/New_LBWApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_LBWApp.swift
+//  New LBW
+//
+//  Created by Home on 3/1/26.
+//
+
+import SwiftUI
+
+@main
+struct New_LBWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
